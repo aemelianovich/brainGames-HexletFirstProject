ce46f0d7f9b2d19e641b44bda641cd7607846c25
Aleskey Emelianovich <aemelianovich@yandex.ru>
2020-07-09 16:44:50 -0500

    Code refactoring.

--- bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import cli from '../src/cli.js';
-import utils from '../src/utils.js';
-
-console.log('Welcome to the Brain Games');
-console.log(`Hello${utils.addCommaPrefixForNonEmptyStr(cli.askUserName())}!`);
--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const askUserName = (question = 'May I have your name? ') => {
-  const userName = readlineSync.question(question);
-  return userName;
-};
-
-export default { askUserName };
